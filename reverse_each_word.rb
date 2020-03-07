bc8e3803e41f08dfed9042ef86094e533ced44ed
Richard Bobo <richard.bobo@gmail.com>
2020-03-07 22:47:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,46 +7,7 @@ def reverse_each_word(string)
 
  result 
  
-#   newArray = string.split(/ /)
-#   i = 0 
-#   while i < newArray.length 
-#   newArray[i] = newArray[i].reverse
-   
-#   i += 1 
-#   end 
-# newArray.join(' ') 
-#   newArray
- 
- 
- 
-# i = 0 
-# while i < newArray.length 
-# newArray[i] = newArray[i].reverse
-# i += 1 
-# end 
-# newArray.reduce 
 
- 
- 
- 
- # newArray.reverse { |newArray| newArray.reverse }
-  
-# string.each do |string| string.reverse  
-  
-  
-
-  
-  #{ |b| b.reverse }
-  
-  
-  #yield
-  #puts string 
 end 
-
-#reverse_each_word("Hello there, and how are you?"){ |b| b.reverse }
-
-#reverse_each_word(string) { |b| b.reverse }
-
-  
 
   
